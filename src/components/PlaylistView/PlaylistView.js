6b2385192625e48f437c472a8f830e2250106ba0
based-on-what <ignacio.pereira@uc.cl>
2024-06-19 10:37:15 -0400

    fix: complete name shown

--- src/components/PlaylistView/PlaylistView.js
+++ src/components/PlaylistView/PlaylistView.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import './PlaylistView.css'; 
-
+import './../../theme.css'; // Importa tu archivo de estilos
 const handlePlaylistClick = (url) => {
-  window.open(url, '_blank'); // Abre la URL en una nueva pestaña
+  window.open(url, '_blank');
 };
 
 const PlaylistView = ({ playlists, isAnimating }) => {
   return (
     <div className={`playlist-grid ${isAnimating ? 'animate-reverse' : ''}`}>
       {playlists.map((playlist) => (
-        <div key={playlist.id} className={`playlist-item border`} style={{ cursor: 'pointer' }} onClick={() => handlePlaylistClick(playlist.url)}>
+        <div 
+          key={playlist.id} 
+          className={`playlist-item border`} 
+          style={{ cursor: 'pointer' }} 
+          onClick={() => handlePlaylistClick(playlist.url)}
+        >
+          <div className="playlist-image">
+            <img src={playlist.image} alt={playlist.name} />
+          </div>
           <div className="playlist-name">
             {playlist.name}
           </div>
-          <img src={playlist.image} alt={playlist.name} className="playlist-image" />
-          <div className="playlist-duration">{`${playlist.duration.days} days, ${playlist.duration.hours} hours, ${playlist.duration.minutes} minutes, ${playlist.duration.seconds} seconds`}</div>
+          <div className="playlist-duration">
+            {`${playlist.duration.days} days, ${playlist.duration.hours} hours, ${playlist.duration.minutes} minutes, ${playlist.duration.seconds} seconds`}
+          </div>
         </div>
       ))}
     </div>
