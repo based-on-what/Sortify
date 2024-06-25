9d51a0d839b75dc0e772e32102fefe2165420ad5
based-on-what <ignacio.pereira@uc.cl>
2024-06-25 00:09:39 -0400

    fix: theme switch

--- src/components/ThemeSwitch/ThemeSwitch.js
+++ src/components/ThemeSwitch/ThemeSwitch.js
@@ -1,18 +1,23 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../../context/ThemeContext';
+import { FaSun, FaMoon } from 'react-icons/fa';
 import './ThemeSwitch.css';
-const ThemeSwitch = ({ id, switchClass, sliderClass }) => {
+
+const ThemeSwitch = ({ id }) => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <label className={`switch ${switchClass}`}>
+    <label className="theme-switch">
       <input
         type="checkbox"
         id={id}
         onChange={toggleTheme}
         checked={theme === 'dark'}
+        style={{ display: 'none' }} // Ocultar el input checkbox
       />
-      <span className={`slider round ${sliderClass}`}></span>
+      <div className="icon-container" onClick={toggleTheme} aria-label="Toggle theme">
+        {theme === 'dark' ? <FaSun /> : <FaMoon />}
+      </div>
     </label>
   );
 };
